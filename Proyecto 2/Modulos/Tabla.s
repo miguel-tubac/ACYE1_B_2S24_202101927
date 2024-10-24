41dc4be13077c6773983f5b3cb6c06e75867cd2d
Miguel Tubac <mgtubac@gmail.com>
2024-10-23 20:22:43 -0600

    Se agrego la operacion de divicion

--- Proyecto 2/Modulos/Tabla.s
+++ Proyecto 2/Modulos/Tabla.s
@@ -6,6 +6,7 @@
 .extern do_suma
 .extern do_resta
 .extern do_multiplica
+.extern do_divicion
 
 .data
     clear:
@@ -45,6 +46,9 @@
     
     comando_multiplicacion:
         .asciz "MULTIPLICACION"
+
+    comando_dividir:
+        .asciz "DIVIDIR"
 
     number64: 
         .word 1000000000  // Definir el número de 32 bits en memoria es decir hasta 10 cifras
@@ -317,8 +321,36 @@ do_tabla:
         b mostrarTabla //Retornamos e implimimos la tabla con los datos actualizados
 
     no_coincide_multi:
-        b end 
+        b comparar_cadena_div 
     //************************************* FIN MULTIPLICACION *********************************************
+
+    //******************************** DIVIDIR *******************************************************
+    comparar_cadena_div:
+        ldr x1, =opcion         // Cargar la dirección de la cadena ingresada
+        ldr x2, =comando_dividir         // Cargar la dirección de la cadena "DIVIDIR"
+        mov x3, #0                   // Inicializar el índice
+        
+    comparar_ciclo_div:
+        ldrb w4, [x1, x3]            // Cargar un carácter de la cadena ingresada
+        ldrb w5, [x2, x3]            // Cargar el carácter correspondiente de "DIVIDIR"
+
+        cmp w4, 32      //Aca se compara con un espacio en blanco y si si entonces ya se termino de leer DIVIDIR
+        BEQ conside_div //Salta a la validacion
+
+        cmp w4, w5                   // Comparar ambos caracteres
+        bne no_coincide_div              // Si no coinciden, saltar a no_match
+
+        cbz w4, conside_div              // Si llegamos al final de ambas cadenas (carácter nulo), son iguales
+        add x3, x3, #1               // Incrementar el índice
+        b comparar_ciclo_div              // Repetir el bucle
+
+    conside_div:
+        bl do_divicion //Salta al archivo Multiplicacion.s en donde se leen los datos
+        b mostrarTabla //Retornamos e implimimos la tabla con los datos actualizados
+
+    no_coincide_div:
+        b end 
+    //************************************* FIN DIVIDIR *********************************************
 
 
 
